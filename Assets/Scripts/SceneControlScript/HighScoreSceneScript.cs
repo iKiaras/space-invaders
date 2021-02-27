20126ccf1d648e80b6b9d75ceef72f6767868056
Ioannis kiaras <ioannis.kiaras@commxp.io>
2021-02-27 21:03:54 +0200

    almost there final

---
+++ Assets/Scripts/SceneControlScript/HighScoreSceneScript.cs
@@ -0,0 +1,26 @@
+﻿using DI;
+using ModestTree;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace SceneControlScript
+{
+    public class HighScoreSceneScript : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI highScoreText;
+        private IHighScores _highScores;
+    
+        void Start()
+        {
+            highScoreText.text = _highScores.GetHighScores().GetHighScoreList().IsEmpty() ? "No Scores At The Moment!" : _highScores.GetHighScores().ToString();
+        }
+
+
+        [Inject]
+        public void SetHighScores(IHighScores highScores)
+        {
+            _highScores = highScores;
+        }
+    }
+}
